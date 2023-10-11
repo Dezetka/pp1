54113edb12a401c8483e3339cb6f9711ef1bf22a
Piotr Skład <146984451+Dezetka@users.noreply.github.com>
2023-10-11 13:22:29 +0200

    Add files via upload

---
+++ My first Python programs from textbook.py
@@ -0,0 +1,30 @@
+#ćwiczenie 2
+
+#name = input ("What is your name?: ")
+#print ("Hello " + name)
+
+#Ćwiczenie 3
+
+#hours = input("Hours: ")
+#rate = input("Rate: ")
+#print(round(float(hours)*float(rate)))
+
+#Ćwiczenie 4
+
+""""
+width = 17
+height = 12.0
+
+print(width//2)
+print(width/2.0)
+print(height/3)
+print(1 + 2 * 5)
+"""
+
+#Ćwiczenie 4
+Celsius = input("Enter the temperature : ")
+fahrenheit = (int(Celsius) * 1.8 + 30)
+print ('The temperature is ' + str(fahrenheit) + ' Fahrenheit degrees')
+    
+
+                
